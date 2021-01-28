c3cd6c8594fc2b4cc7d0756ebecf1efca26bafbf
Quazi Marufur Rahman <quazi.rahman@qut.edu.au>
2021-01-28 12:03:18 +1000

    config file

---
+++ src/config.py
@@ -0,0 +1,5 @@
+VIDEO_PATH='../data/video.mp4'
+VIDEO_POLYGON_PATH='../data/video_polygon.json'
+FRAME_POLYGON_PATH='../data/polygon.json'
+FRAME_OBJECT_PATH='../data/object.json'
+FRAME_PATH='../data/image.png'
